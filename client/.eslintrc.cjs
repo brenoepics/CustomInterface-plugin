d372a4ed80b6841972e590d3f8ea64333ba631a4
BrenoEpic <github@brenoepic.tech>
2024-01-08 01:17:52 -0300

    Add Eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,17 +1,6 @@
 require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
-   "env": {
-      "browser": true,
-      "commonjs": true,
-      "es6": true,
-      "node": true
-   },
-   "parser": "vue-eslint-parser",
-   "parserOptions": {
-      "parser": "@typescript-eslint/parser",
-      "sourceType": "module"
-    },
     extends: [
       'eslint:recommended',
       'plugin:vue/essential',
