ff1bf33e7aa6e2de0c2b68efb05aed163aa11c54
BrenoEpic <github@brenoepic.tech>
2024-01-08 00:41:40 -0300

    Update RaresValueEvent.ts

--- client/src/communication/incoming/general/RaresValueEvent.ts
+++ client/src/communication/incoming/general/RaresValueEvent.ts
@@ -1,7 +1,33 @@
+import Rare from '@/store/models/Rare';
 import IncomingMessage from '../IncomingMessage';
+import App from '@/App';
+import RareCategory from '@/store/models/RareCategory';
 
+interface Data {
+    rares: {
+        id: number;
+        name: string;
+        image: string;
+        category: number;
+        cost: {
+            credits: number;
+            points: number;
+            pointsType: number;
+        }
+    }[];
+    categories: {
+        id: number;
+        name: string;
+    }[];
+    owned: number[];
+    frontpage: string;
+}
 export default class RaresValueEvent implements IncomingMessage {
-    parse(data: any): void {
-        console.log(data);
+    parse(data: Data): void {
+        App.interfaceManager.container.$store.commit('rarevalues/setRares', data.rares.map(rare => new Rare(rare.id, rare.name, rare.image, rare.category, rare.cost)));
+        App.interfaceManager.container.$store.commit('rarevalues/setCategories', data.categories.map(category => new RareCategory(category.id, category.name)));
+        App.interfaceManager.container.$store.commit('rarevalues/setOwned', data.owned);
+        App.interfaceManager.container.$store.commit('rarevalues/setFrontpage', data.frontpage);
+        App.interfaceManager.container.$store.commit('rarevalues/setOpen', true);
     }
 }
